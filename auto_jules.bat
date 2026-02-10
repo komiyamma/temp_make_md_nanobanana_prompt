3408c09c635977a13d136ade59a1017b55cfb9c8
komiyamma <komiyama0629@gmail.com>
2026-02-10 12:42:13 +0900

    chore(auto_jules): generate_list後に待機時間を追加

--- auto_jules.bat
+++ auto_jules.bat
@@ -1,6 +1,7 @@
 cd /d "%~dp0"
 pwsh -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0generate_list.ps1"
 if errorlevel 1 exit /b %errorlevel%
+timeout /t 5 /nobreak
 git add -A
 git commit -m "addition"
 git push
